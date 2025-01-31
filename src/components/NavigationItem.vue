e615425fbef4f3824cbd03685c72206fc6a4421f
Alan Uecker <uecker.alan@gmail.com>
2025-01-31 15:46:42 +0100

    fixed issue with link value

--- src/components/NavigationItem.vue
+++ src/components/NavigationItem.vue
@@ -7,7 +7,7 @@ defineProps<{ label: string; href: string }>();
 <template>
   <li>
     <RouterLink
-      to="href"
+      :to="href"
       class="text-dark disabled:text-dark-500 inline-flex items-center justify-center rounded-md px-6 py-2
         text-center text-base font-medium hover:bg-gray-300 disabled:bg-gray-200 md:w-full md:justify-start"
     >
